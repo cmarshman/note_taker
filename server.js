c5e6b25bfc6dabc3bcf08c72d079883d819f38ce
Cory Marshman <marshman.cory@gmail.com>
2020-04-02 23:26:49 -0400

    route and server fixes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function(){
-    console.log("app listening on port: " + PORT);
+    console.log("app listening on port: " + PORT);             
 });
 
 
